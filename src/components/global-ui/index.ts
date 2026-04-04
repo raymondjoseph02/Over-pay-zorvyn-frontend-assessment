474be3e30763f04706c3bcb2306cc4d4a2c1efd9
raymondjoseph02 <amraymondjoseph@gmail.com>
2026-04-04 15:31:48 +0100

    fix:fix build error

--- src/components/global-ui/index.ts
+++ src/components/global-ui/index.ts
@@ -1,7 +1,6 @@
 export { Header } from "./Header";
 export { NavBar } from "./NavBar";
 export { SideBar } from "./SideBar";
-export { ProfileModal } from "../modals/ProfileModal";
 export { SideBarLink } from "./SidbarLinks";
 export { Button } from "./Button";
 export * from "./Skeleton";
